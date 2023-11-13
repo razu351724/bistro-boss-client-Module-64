cc957895fc274b26147a234f84dbf13544374561
RAZU-sheikh <razu351724@gmail.com>
2023-11-14 03:12:30 +0600

    Show Popular menu items in the home page show

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import SwiperHome from "../Category/SwiperHome";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <SwiperHome></SwiperHome>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
