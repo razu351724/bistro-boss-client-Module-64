df0e2c2bc632f2ca50a898a105d43c5f7f8b4597
RAZU-sheikh <razu351724@gmail.com>
2023-12-08 20:43:48 +0600

    Load all users on the Dashboard page || Display All users and Create Make admin API || Make User Admin and install JWT

--- src/Layout/Dashbord.jsx
+++ src/Layout/Dashbord.jsx
@@ -1,39 +1,74 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaSearch, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 const Dashbord = () => {
     const [cart] = useCart();
+
+    // TODO: get isAdmin value from the database
+    const isAdmin = true;
+
     return (
         <div className="flex">
             {/* dashboard side bar */}
             <div className="w-64 min-h-screen bg-orange-400">
                 <ul className="menu">
-                    <li>
-                        <NavLink to='/dashboard/userHome'>
-                            <FaHome></FaHome>
-                            User Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/reservation'>
-                            <FaCalendar></FaCalendar>
-                            Reservation</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/cart'>
-                            <FaShoppingCart></FaShoppingCart>
-                            My Cart ({cart.length})</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/review'>
-                            <FaAd></FaAd>
-                            Add a Review</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/bookings'>
-                            <FaList></FaList>
-                            My Bookings</NavLink>
-                    </li>
+                    {
+                        isAdmin ? <>
+                            <li>
+                                <NavLink to='/dashboard/adminHome'>
+                                    <FaHome></FaHome>
+                                    Admin Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/addItems'>
+                                    <FaUtensils></FaUtensils>
+                                    Add Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/manageItems'>
+                                    <FaList></FaList>
+                                    Manage Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/bookings'>
+                                    <FaBook></FaBook>
+                                    Manage Bookings</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/allUsers'>
+                                    <FaUsers></FaUsers>
+                                    All Users</NavLink>
+                            </li>
+                        </> : <>
+                            <li>
+                                <NavLink to='/dashboard/userHome'>
+                                    <FaHome></FaHome>
+                                    User Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/reservation'>
+                                    <FaCalendar></FaCalendar>
+                                    Reservation</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/cart'>
+                                    <FaShoppingCart></FaShoppingCart>
+                                    My Cart ({cart.length})</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/review'>
+                                    <FaAd></FaAd>
+                                    Add a Review</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/dashboard/bookings'>
+                                    <FaList></FaList>
+                                    My Bookings</NavLink>
+                            </li>
+                        </>
+                    }
+                    {/* shared nav links */}
                     <div className="divider"></div>
                     <li>
                         <NavLink to='/'>
@@ -44,6 +79,11 @@ const Dashbord = () => {
                         <NavLink to='/order/salad'>
                             <FaSearch></FaSearch>
                             Menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/order/contact'>
+                            <FaEnvelope></FaEnvelope>
+                            Contact</NavLink>
                     </li>
                 </ul>
             </div>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000/carts'
+    baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
